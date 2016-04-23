8c5adbcae7003cfd44b8c4e4cb682a6ca8970f8a
Martin Romero <martin_ale_romero@hotmail.com>
2016-04-23 19:28:16 -0300

    cambio de comentario sobre los temas

--- app/src/main/java/nqn/app/martinromero/example/com/myappmr/SecondActivity.java
+++ app/src/main/java/nqn/app/martinromero/example/com/myappmr/SecondActivity.java
@@ -61,7 +61,7 @@ public class SecondActivity extends AppCompatActivity implements OnClickListener
             texto.setText(dato);
         }
 
-        //aca manejo mi lista personalizada
+        //aca manejo mi lista personalizada de temas
         Bandas bandas_data[] = new Bandas[]{
                 new Bandas(R.drawable.ic_launcher,"ColdPlay"),
                 new Bandas(R.drawable.ic_launcher,"Limp Bizkit"),
